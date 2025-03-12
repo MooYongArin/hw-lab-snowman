547f0ac1f15f0c3fc62e47c93f49f6a1b7871920
Sippakorn-th <sippakorn.th@gmail.com>
2025-03-12 12:05:59 +0700

    complete ALUCtrl.v

--- Lab6/src/ALUCtrl.v
+++ Lab6/src/ALUCtrl.v
@@ -33,39 +33,39 @@ localparam ADDIFNLT = 4'b1010;
 
 always @(*) begin 
     if (funct7 == 0 && funct3 == 3'b000 && ALUOp == 0) begin // ADD
-
+        ALUCtl <= ADD;
     end else if (funct3 == 3'b000 && ALUOp == 1) begin //ADDI
-
+        ALUCtl <= ADD;
     end else if (funct7 == 1 && funct3 == 3'b000 && ALUOp == 0) begin //SUB
-
+        ALUCtl <= SUB;
     end else if (funct7 == 0 && funct3 == 3'b111 && ALUOp == 0) begin //AND
-
+        ALUCtl <= AND;
     end else if (funct3 == 3'b111 && ALUOp == 1) begin //ANDI
-
+        ALUCtl <= AND;
     end else if (funct7 == 0 && funct3 == 3'b110 && ALUOp == 0) begin //OR
-
+        ALUCtl <= OR;
     end else if (funct3 == 3'b110 && ALUOp == 1) begin //ORI
-
+        ALUCtl <= OR;
     end else if (funct7 == 0 && funct3 == 3'b010 && ALUOp == 0) begin //SLT
-
+        ALUCtl <= SLT;
     end else if (funct3 == 3'b010 && ALUOp == 1) begin //SLTI
-
+        ALUCtl <= SLT;
     end else if (ALUOp == 2) begin //LW
-
+        ALUCtl <= ADD;
     end else if (ALUOp == 3) begin //SW
-
+        ALUCtl <= NOTHING;
     end else if (funct3 == 3'b000 && ALUOp == 4) begin //BEQ
-
+        ALUCtl <= ADDIFEQ;
     end else if (ALUOp == 5) begin //JAL
-
+        ALUCtl <= ADD;
     end else if (ALUOp == 6) begin //JALR
-
+        ALUCtl <= ADDEVEN;
     end else if (funct3 == 3'b001 && ALUOp == 4) begin //BNE
-
+        ALUCtl <= ADDIFNEQ;
     end else if (funct3 == 3'b100 && ALUOp == 4) begin //BLT
-
+        ALUCtl <= ADDIFLT;
     end else if (funct3 == 3'b101 && ALUOp == 4) begin //BGE
-
+        ALUCtl <= ADDIFNLT;
     end 
 
 
