c4adb8df9bbe041ad9ac27c37be7d769d9622958
Sippakorn-th <sippakorn.th@gmail.com>
2025-03-18 00:28:32 +0700

    fix PC.v

--- Lab6/src/PC.v
+++ Lab6/src/PC.v
@@ -1,4 +1,4 @@
-module PC (
+/*module PC (
     input clk,              // clock
     input rst,              // active low reset
     input [31:0] pc_i,      // input program counter (value which will be assigned to PC)
@@ -23,6 +23,26 @@ module PC (
         
     end
 
+
+endmodule
+*/
+module PC (
+    input clk,          // clock
+    input rst,          // active low reset
+    input [31:0] pc_i,  // input program counter (value which will be assigned to PC)
+    output reg [31:0] pc_o // output program counter
+);
+
+    // TODO: implement your program counter here
+    // Hint: If reset is low, assign PC to zero
+
+    always @(posedge clk or negedge rst) begin
+        if (~rst) begin 
+            pc_o <= 32'h0; 
+        end else begin
+            pc_o <= pc_i; 
+        end
+    end
 
 endmodule
 
